37bda91e3d670a7cddba733c08273a2841a9694b
Piotr Nawrocki <piotr_naw99@outlook.com>
2024-03-15 23:05:48 +0100

    15032024 Dodanie middelware admin do Kerner.php, Utworzenie HomeController i dwóch klas idex oraz post, zmiany w plikach typu blade oraz w routingu

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,10 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/home', [HomeController::class, 'index'])->middleware('auth')->name('home');
+
+Route::get('/post', [HomeController::class, 'post'])->middleware(['auth', 'admin'])->name('post');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
